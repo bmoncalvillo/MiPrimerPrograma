cc77c23878f6fcb6ddd2bf94b7616e8003b4fa78
LitzyBarroz <38784287+LitzyBarroz@users.noreply.github.com>
2019-06-21 12:33:56 +0200

    Initial commit

---
+++ src/Formi.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Formi {
+    private JPanel panel1;
+    private JButton button1;
+    private JCheckBox checkBox1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Formi");
+        frame.setContentPane(new Formi().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public Formi() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                button1.setText("Cambiado");
+            }
+        });
+        checkBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Introduce algo");
+        Scanner sc=new Scanner(System.in);
+
+        String cadena=sc.nextLine();
+        ArrayList<Integer> mio=new ArrayList<>();
+
+        var add = mio.add(1);
+
+
+    }
+}
